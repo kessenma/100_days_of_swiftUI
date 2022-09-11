ff5641f5f851890c7122c3b512ea81918cd9b33d
Kyle Essenmacher <kessenmacher7832@gmail.com>
2022-09-11 00:08:57 -0400

    first commit

---
+++ Day4_Checkpoint2.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+// the challenge is to:
+// 1. create an array of strings
+// 2. then write some code that prints the number of items in the array and also the number of unique items in the array.
+
+let housewives = ["Lisa", "Brandi", "Taylor", "Kyle", "Kim", "Adriene", "Camille", "Yolanda"]
+let set = Set(housewives)
+
+var result = housewives.count
+var UniqueResult = housewives
+
+print (result)
+print(set.count) 
+
+// I've been watching real housewives of beverly hills..... my brain is mush
+
